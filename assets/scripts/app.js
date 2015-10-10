b17bd612d6772b5516ca957a2d9552d702a38627
Ana Isabel Carvalho <ana.isabelc@gmail.com>
2015-10-10 12:54:03 +0100

    Formatei melhor os links das contas sociais. Adicionei imagem para preview da url :D

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,10 @@
 $(document).foundation();
 
+// SVG / PNG
+  if(!Modernizr.svg) {
+    $('img[src*="svg"]').attr('src', function () {
+        return $(this).attr('src').replace('.svg', '.png');
+    });
+  }
+
 console.log("ok funciona o reload");
