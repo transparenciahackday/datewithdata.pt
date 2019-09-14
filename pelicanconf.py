f83830da3ce34d50bb0f304a5aae7c4597d56964
Ricardo Lafuente <r@manufacturaindependente.org>
2019-09-14 14:31:39 +0100

    Mudar para uma base Pelican

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from __future__ import unicode_literals
+
+AUTHOR = 'Transparência Hackday Portugal'
+SITENAME = 'Date With Data'
+SITEURL = ''
+
+PATH = 'content'
+THEME = 'theme/datewithdata'
+
+TIMEZONE = 'Europe/Lisbon'
+
+DEFAULT_LANG = 'pt'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
