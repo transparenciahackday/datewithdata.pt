5324b9c9610bc2526ebe111f3b86651e6c3c2c00
Ana Isabel Carvalho <ana@localhost.lan.lan.lan.lan.lan.lan.lan.lan.lan.lan.lan.lan.lan>
2019-09-14 23:12:03 +0000

    A incluir conteudos nos templates base, index e article.

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 from __future__ import unicode_literals
 
+''' Content '''
 AUTHOR = 'Transparência Hackday Portugal'
 SITENAME = 'Date With Data'
-SITEURL = ''
+SITEURL = 'http://datewithdata.pt'
+SITE_DESCRIPTION = "A lançar dados desde 2010"
+
+TIMEZONE = 'Europe/Lisbon'
+DEFAULT_LANG = 'pt'
 
 PATH = 'content'
 THEME = 'theme/datewithdata'
 
-TIMEZONE = 'Europe/Lisbon'
-
-DEFAULT_LANG = 'pt'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -33,3 +35,16 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Set up clean URLs
+ARTICLE_URL = '{category}/{slug}/'
+ARTICLE_SAVE_AS = '{category}/{slug}/index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+CATEGORY_URL = '{slug}/'
+CATEGORY_SAVE_AS = '{slug}/index.html'
+DRAFT_URL = 'private/{slug}/'
+DRAFT_SAVE_AS = 'private/{slug}/index.html'
+DRAFT_LANG_URL = 'private/{slug}-{lang}/'
+DRAFT_LANG_SAVE_AS = 'private/{slug}-{lang}/index.html'
+
