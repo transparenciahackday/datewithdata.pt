1f75e95ea6d17bcca4b34b76b0c0810db8bc1001
Ricardo Lafuente <r@manufacturaindependente.org>
2019-09-21 15:00:02 +0100

    Clean config file

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,24 +13,6 @@ DEFAULT_LANG = 'pt'
 PATH = 'content'
 THEME = 'theme/datewithdata'
 
-
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
@@ -47,4 +29,11 @@ DRAFT_URL = 'private/{slug}/'
 DRAFT_SAVE_AS = 'private/{slug}/index.html'
 DRAFT_LANG_URL = 'private/{slug}-{lang}/'
 DRAFT_LANG_SAVE_AS = 'private/{slug}-{lang}/index.html'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
