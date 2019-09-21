a1f05a0fb0b167d7866610ab0826a1d56457e34f
Ana Isabel Carvalho <ana@localhost.lan>
2019-09-21 14:45:20 +0000

    Adicionar templates, corrigir conf e um pouco de css

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 ''' Content '''
 AUTHOR = 'Transparência Hackday Portugal'
 SITENAME = 'Date With Data'
-SITEURL = 'http://datewithdata.pt'
+SITEURL = ''
 SITE_DESCRIPTION = "A lançar dados desde 2010"
 
 TIMEZONE = 'Europe/Lisbon'
@@ -16,7 +16,7 @@ THEME = 'theme/datewithdata'
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 # Set up clean URLs
 ARTICLE_URL = '{category}/{slug}/'
